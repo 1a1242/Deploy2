b8b6aa81c8ce8f50de0493b9e1b51eba0dbf298f
1a1242 <devisriya26@gmail.com>
2022-04-01 11:47:53 +0530

    Port change

--- node/frontend/index.js
+++ node/frontend/index.js
@@ -46,9 +46,7 @@ app.get("/", (req, res)=>{
     res.send({success: true, msg: "Hello World"});
 })
 
-app.listen(3000, ()=>{
-    console.log("Server started on 3000")
-})
+app.listen(process.env.PORT || 3000);
 
 // const express = require('express')
 // const app = express()
